15245a4c4d5a5eff97f9ccc9b23c3294ad89ecff
kys0554 <kys0554@gmail.com>
2025-04-02 11:24:39 +0900

    first

---
+++ test123/project/src/main/java/com/test/project/BoardController.java
@@ -0,0 +1,17 @@
+package com.test.project;
+
+public class BoardController {
+	public static void main(String[] args) {
+	
+	}
+	public void abc() {
+		System.out.println("гоюл");
+	}
+	public void zzz () {
+		
+	}
+
+	public void ccc() {
+		
+	}
+}
